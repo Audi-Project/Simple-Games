6c1a1ac97d1494f8f86659313dec08bf3f199ffd
ddaeunbb <ddaeunbb@gmail.com>
2023-11-30 14:09:10 +0900

    :sparkles: Default-settings
    emotion 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
-
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, adipisci
+      mollitia veritatis autem, officia vel nemo ea possimus velit consectetur
+      repudiandae quaerat quasi iste incidunt error modi earum ducimus
+      laudantium?
+    </main>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
+import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        ${emotionReset}
+        @font-face {
+          font-family: 'GmarketSansMedium';
+          src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff')
+            format('woff');
+          font-weight: normal;
+          font-style: normal;
+          color: '#233868';
+        }
+
+        *,
+        *::after,
+        *::before {
+          font-family: 'GmarketSansMedium';
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
 );
